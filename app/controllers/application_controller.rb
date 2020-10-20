b561c2e7bfc1dc5be2e484a0490cdce729815171
Thang ninh <thang13199@gmail.com>
2020-10-21 01:42:38 +0700

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 # fix rubocop
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
