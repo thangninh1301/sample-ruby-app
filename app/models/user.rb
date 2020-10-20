80acb8f1e6b643be3695b0ae850d41150dd89b74
Thang ninh <thang13199@gmail.com>
2020-10-20 08:33:15 +0700

    Password_reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -32,6 +32,10 @@ class User < ApplicationRecord
   def session_token
     remember_digest || remember
   end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
   # Activates an account.
   def activate
     update_attribute(:activated, true)
@@ -40,6 +44,17 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   def authenticated?(attribute, token)
