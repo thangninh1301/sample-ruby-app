ef1a86c1ada02a7eee37a68a4dda344fbbc91ebd
Thang ninh <thang13199@gmail.com>
2020-12-01 16:07:10 +0700

    finish mail

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,6 @@ class Notification < ApplicationRecord
   belongs_to :source, polymorphic: true
   after_create_commit do
     NotificationBroadcastJob.perform_later(self, user_id)
+    UserMailer.notification(self).deliver_later
   end
 end
