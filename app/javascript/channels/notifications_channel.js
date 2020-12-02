7d873933980c3aa8e2495ac72d2aeef3b9460a1c
Thang ninh <thang13199@gmail.com>
2020-12-02 09:45:10 +0700

    add gitignore

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -3,7 +3,6 @@ import consumer from "./consumer"
 consumer.subscriptions.create("NotificationsChannel", {
     connected() {
         // Called when the subscription is ready for use on the server
-        console.log('connected to action cable')
     },
 
     disconnected() {
--- spec/factories/notifications.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :notification do
-    event { 'MyString' }
-  end
-end
