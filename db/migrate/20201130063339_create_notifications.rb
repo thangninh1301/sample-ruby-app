a93ac85b8c5b7539b290975d1d6aa6efd38366d5
Thang ninh <thang13199@gmail.com>
2020-12-02 08:56:26 +0700

    add mail preview

--- db/migrate/20201130063339_create_notifications.rb
+++ db/migrate/20201130063339_create_notifications.rb
@@ -1,7 +1,6 @@
 class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
-      t.string :event
       t.references :user, null: false, foreign_key: true
       t.integer :source_id
       t.string :source_type
