9513e7e0742e940767df2b7d0fef9836696a23c6
Thang ninh <thang13199@gmail.com>
2020-12-02 09:18:39 +0700

    del unuse console.log

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -12,7 +12,6 @@ consumer.subscriptions.create("NotificationsChannel", {
 
     received(data) {
         // Called when there's incoming data on the websocket for this channel
-        console.log(data)
         $('#notifications-counter').html(data.counter)
         $('#notifications-list').prepend(data.notification)
     }
