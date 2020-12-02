c7645df86f2a0b4f0e67265027b7e03fbbd3d14c
Thang ninh <thang13199@gmail.com>
2020-12-02 08:18:40 +0700

    reformat code

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -1,19 +1,19 @@
 import consumer from "./consumer"
 
 consumer.subscriptions.create("NotificationsChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log('connected to action cable')
-  },
+    connected() {
+        // Called when the subscription is ready for use on the server
+        console.log('connected to action cable')
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+        // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log(data)
-    $('#notifications-counter').html(data.counter)
-    $('#notifications-list').prepend(data.notification)
-  }
+    received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        console.log(data)
+        $('#notifications-counter').html(data.counter)
+        $('#notifications-list').prepend(data.notification)
+    }
 });
