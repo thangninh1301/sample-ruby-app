55e6a3803cd582487b6a3a32f5067d5acca71f10
Thang ninh <thang13199@gmail.com>
2020-12-03 08:54:15 +0700

    change update

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,17 +3,15 @@ class NotificationsController < ApplicationController
   before_action :correct_user, only: %i[update show]
 
   def update
-    @notification.is_seen = true
-    @notification.save
-  end
-  respond_to do |format|
-    format.html { to_last_url }
-    format.js {}
+    @notification.update(is_seen: true)
+    respond_to do |format|
+      format.html { to_last_url }
+      format.js {}
+    end
   end
 
   def show
-    @notification.is_seen = true
-    @notification.save
+    @notification.update(is_seen: true)
     return redirect_to @notification.source.react_to.micropost.user if @notification.source_type == 'Reaction'
 
     redirect_to @notification.source.micropost.user
@@ -25,6 +23,4 @@ class NotificationsController < ApplicationController
     @notification = current_user.notifications.find_by(id: params[:id])
     redirect_to root_url if @notification.nil?
   end
-
-  def micropost_url; end
 end
