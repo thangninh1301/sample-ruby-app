e688d8c7d30e2b8b27b2ff2e531d589a4283e980
thang <thang@Thangs-MacBook-Pro.local>
2020-12-22 13:24:48 +0700

    finish:

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,13 +4,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [800, 800]
 
-  version :fit_message_box do
-    process resize_to_fill: [200, 200]
-  end
+  # version :message_box do
+  #   process resize_to_fill: [200, 200]
+  # end
 
   storage :file
 
-  if Rails.env.development? || Rails.env.production?
+  unless Rails.env.test? || Rails.env.cucumber?
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
