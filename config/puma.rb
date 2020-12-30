61b3295c3b373e284e554a5e876f6555159d8ff0
thang <thang@Thangs-MacBook-Pro.local>
2020-12-30 10:50:11 +0700

    p

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,11 @@
-# Change to match your CPU core count
-workers 1
-
-# Min and Max threads per worker
-threads 1, 6
-
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-
-# Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
-environment rails_env
-
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
-
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
-
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
+app_dir     = File.expand_path("../..", __FILE__)
+shared_dir  = "#{app_dir}/shared"
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+threads min_threads_count, max_threads_count
+port ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RAILS_ENV') { ENV['RACK_ENV'] || 'development' }
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+preload_app!
+plugin :tmp_restart
