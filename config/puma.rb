4a9efad21b4d9097d7414b519a7c11390ed7cadb
thang <thang@Thangs-MacBook-Pro.local>
2020-12-31 11:17:35 +0700

    edit puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,10 @@
-# Puma configuration file.
+app_dir     = File.expand_path("../..", __FILE__)
+shared_dir  = "#{app_dir}/public"
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 port ENV.fetch('PORT') { 3000 }
+bind "unix://#{shared_dir}/puma.sock"
 environment ENV.fetch('RAILS_ENV') { ENV['RACK_ENV'] || 'development' }
 pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 workers ENV.fetch('WEB_CONCURRENCY') { 2 }
