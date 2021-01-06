3d6598b93eaac7d1c78f508c6ede2ea7bb58cf3c
thang <thang@Thangs-MacBook-Pro.local>
2021-01-06 10:20:05 +0700

    finish

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "sample_app"
-set :repo_url, "git@github.com:thangninh1301/sample_app.git"
-
 set :ssh_options, { :forward_agent => true }
 
 set :pty, true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,6 @@ set :branch, "dplTask"
 set :deploy_to, "/home/deploy/sample_app"
 server "54.255.48.27", user: "deploy", roles: %w{app db web}
 
+set :application, "sample_app"
+set :repo_url, "git@github.com:thangninh1301/sample_app.git"
+
