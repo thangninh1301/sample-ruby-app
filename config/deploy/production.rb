7cd887f54154edad610b24c5a48f0a4549003c59
thang <thang@Thangs-MacBook-Pro.local>
2021-01-07 12:04:13 +0700

    http to host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :stage, :production
 set :rails_env, :production
 set :branch, "dplTask"
 set :deploy_to, "/home/deploy/sample_app"
-server "54.255.48.27", user: "deploy", roles: %w{app db web}
+server "13.212.214.174", user: "deploy", roles: %w{app db web}
 
 set :application, "sample_app"
 set :repo_url, "git@github.com:thangninh1301/sample_app.git"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = "ec2-13-212-214-174.ap-southeast-1.compute.amazonaws.com"
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http'  }
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.gmail.com',
       :port => '587',
