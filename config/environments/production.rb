954954ed32d4f634ac53420759382d209949e768
thang <thang@Thangs-MacBook-Pro.local>
2021-01-07 11:27:41 +0700

    add mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "ec2-54-179-86-156.ap-southeast-1.compute.amazonaws.com"
+  host = "ec2-13-212-214-174.ap-southeast-1.compute.amazonaws.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.sendgrid.net',
       :port => '587',
       :authentication => :plain,
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'heroku.com',
+      :user_name => ENV['MAIL_USERNAME'],
+      :password => ENV['MAIL_PASSWORD'],
       :enable_starttls_auto => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
